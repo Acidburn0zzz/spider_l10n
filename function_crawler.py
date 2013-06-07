b5d73c31e44f021c46398c13eaec72459249d2fd
SphinxKnight <julien.gattelier@gmail.com>
2013-06-07 14:42:58 +0200

    Pylint has come + small error

--- function_crawler.py
+++ function_crawler.py
@@ -1,153 +1,155 @@
-import re
-import heapq
-import urllib.robotparser
-from urllib.parse import urlparse
-import sys 
-import string 
+from re              import findall, match, search, compile,DOTALL
+from heapq           import heappush
+from urllib          import robotparser
+from urllib.parse    import urlparse
 
 
 
 def isFetchable(pageURL, rp, agentName, domainLimit):
-	
-	#Test 1 : is the page outside the domain 
-	if not(pageURL.startswith(domainLimit)) :
-		#print("An out of domain link has been found")
-		#print("The link is : "+pageURL)
-		return False
+    
+    #Test 1 : is the page outside the domain 
+    if not(pageURL.startswith(domainLimit)) :
+        #print("An out of domain link has been found")
+        #print("The link is : "+pageURL)
+        return False
 
-	#Test 2 : is the page not allowed via robots.txt	
-	if not(rp.can_fetch(agentName,pageURL)):
-		return False
+    #Test 2 : is the page not allowed via robots.txt    
+    if not(rp.can_fetch(agentName, pageURL)):
+        return False
 
-	return True
-	
+    return True
+    
 
 
 def getListLinks(contentStr):
-	return re.findall('<a(?:.*?)href="(.*?)"(?:.*?)>(?:.*?)</a>', contentStr)
-	
+    return findall('<a(?:.*?)href="(.*?)"(?:.*?)>(?:.*?)</a>', contentStr)
+    
 
 def computeDepth(absoluteURL):
-	#How deep is our URL
-	return str.count(urlparse(absoluteURL).path,"/")
+    #How deep is our URL
+    return str.count(urlparse(absoluteURL).path, "/")
 
-		
-		
+        
+        
 def checkURL(url):
-	
-	return re.match("^(http://|https://){0,1}[A-Za-z0-9][A-Za-z0-9\-\.]+[A-Za-z0-9]\.[A-Za-z]{2,}[\43-\176]*$",url)
+    
+    return match("^(http://|https://){0,1}[A-Za-z0-9][A-Za-z0-9\-\.]+[A-Za-z0-9]\.[A-Za-z]{2,}[\43-\176]*$",url)
 
 def filteredLink(link):
-	#Change here if another setting
-	#Set of filters to implement
-	return ("$revision" in link) or ("$history" in link)	or ("$edit" in link) or ("$locale" in link) or (link=="https://developer.mozilla.org/en-US/docs/new")
+    #Change here if another setting
+    #Set of filters to implement
+    test = ("$revision" in link)
+    test = test or ("$history" in link)
+    test = test or ("$edit" in link)
+    test = test or ("$locale" in link)
+    test = test or (link=="https://developer.mozilla.org/en-US/docs/new")
+    return  test
 
 def readFile(filename):
-	f = open(filename, 'r')
-	try:
-	    content = f.read()
-	finally:
-	    f.close()
-	return content
+    f = open(filename, 'r')
+    try:
+        content = f.read()
+    finally:
+        f.close()
+    return content
 
 def saveFile(data, filename):
-	string = ''
-	array = []
-	f = open(filename, 'w')
-	try:
-		if type(data) == type(string):
-			f.write(data)
-		if type(data) == type(array):
-			for d in data:
-				f.write(str(d) + '\n')
-	finally:
-		f.close()
-def saveData(data,f):
-	string = ''
-	array = []
-	if type(data) == type(string):
-			f.write(data)
-	if type(data) == type(array):
-		for d in data:
-			f.write(str(d) + '\n')
+    string = ''
+    array = []
+    f = open(filename, 'w')
+    try:
+        if type(data) == type(string):
+            f.write(data)
+        if type(data) == type(array):
+            for d in data:
+                f.write(str(d) + '\n')
+    finally:
+        f.close()
+def saveData(data, f):
+    string = ''
+    array = []
+    if type(data) == type(string):
+            f.write(data)
+    if type(data) == type(array):
+        for d in data:
+            f.write(str(d) + '\n')
 
 def saveState(frontier,exploredPages,listePagesStatus,listErrorPages,counterNT,domainLimit,addressRobot,crawlDelay,targetLanguage):
 
-	f = open("savedState.txt", 'w')
-	try:
-		#Save frontier
-		f.write("--Frontier--\n")
-		saveData(frontier,f)
-		#Save exploredPages
-		f.write("--Explored pages--\n")
-		saveData(exploredPages,f)
-		#Save listePagesStatus
-		f.write("--List of pages status--\n")
-		saveData(listePagesStatus,f)
-		#Save listeErrorPages
-		f.write("--List of error pages--\n")
-		saveData(listErrorPages,f)
-		#Save counterNT
-		f.write("--Counter of non-translated--\n")
-		saveData(str(counterNT),f)
-		#Save domainLimit
-		f.write("\n--Limit of domain to crawl--\n")
-		saveData(domainLimit,f)
-		#Save addressRobot
-		f.write("\n--Robots file address--\n")
-		saveData(addressRobot,f)
-		#Save crawlDelay
-		f.write("\n--Crawl delay chosen--\n")
-		saveData(crawlDelay,f)
-		f.write("\n--Target language--\n")
-		saveData(targetLanguage,f)
-	finally:
-		f.close()
+    f = open("savedState.txt", 'w')
+    try:
+        #Save frontier
+        f.write("--Frontier--\n")
+        saveData(frontier,f)
+        #Save exploredPages
+        f.write("--Explored pages--\n")
+        saveData(exploredPages,f)
+        #Save listePagesStatus
+        f.write("--List of pages status--\n")
+        saveData(listePagesStatus,f)
+        #Save listeErrorPages
+        f.write("--List of error pages--\n")
+        saveData(listErrorPages,f)
+        #Save counterNT
+        f.write("--Counter of non-translated--\n")
+        saveData(str(counterNT),f)
+        #Save domainLimit
+        f.write("\n--Limit of domain to crawl--\n")
+        saveData(domainLimit,f)
+        #Save addressRobot
+        f.write("\n--Robots file address--\n")
+        saveData(addressRobot,f)
+        #Save crawlDelay
+        f.write("\n--Crawl delay chosen--\n")
+        saveData(str(crawlDelay),f)
+        f.write("\n--Target language--\n")
+        saveData(targetLanguage,f)
+    finally:
+        f.close()
 
 def loadState(frontier,exploredPages,listePagesStatus,listErrorPages,counterNT,domainLimit,addressRobot,crawlDelay,targetLanguage):
-	state=readFile("savedState.txt")
-	#Load frontier
-	listFrontier = re.search('--Frontier--\n(.*)--Explored pages--\n', state,re.DOTALL).group(1).split('\n')[:-1]
-	frontier=[]
-	for elem in listFrontier:
-		score= elem[2:-2].split(",")[0]
-		page= elem[2:-2].split(",")[1][2:]
-		heapq.heappush(frontier,(score,page))
-	exploredPages = re.search('--Explored pages--\n(.*)--List of pages status--\n', state,re.DOTALL).group(1).split('\n')[:-1] #list of links
-	rawListePagesStatus = re.search('--List of pages status--\n(.*)--List of error pages--\n', state,re.DOTALL).group(1).split('\n')[:-1]
-	listePagesStatus =[] #List of (link,status)
-	for elem in rawListePagesStatus:
-		link= elem[2:-2].split("', '")[0]
-		status= elem[2:-2].split("', '")[1]
-		listePagesStatus.append((link,status))
-	listErrorPages=re.search('--List of error pages--\n(.*)--Counter of non-translated--\n', state,re.DOTALL).group(1).split('\n')[:-1] #List of links
-	counterNT=int(re.search('--Counter of non-translated--\n(.*)--Limit of domain to crawl--\n', state,re.DOTALL).group(1))
-	domainLimit = re.search('--Limit of domain to crawl--\n(.*)--Robots file address--\n', state,re.DOTALL).group(1) #String (url)
-	addressRobot = re.search('--Robots file address--\n(.*)--Crawl delay chosen--\n', state,re.DOTALL).group(1) #String (url)
-	crawlDelay=int(re.search('--Crawl delay chosen--\n(.*)--Target language--\n', state,re.DOTALL).group(1))
-	targetLanguage = re.search('--Target language--\n(.*)', state,re.DOTALL).group(1) #String 
-	#Create Dict and add return
-	dicoResult = dict()
-	dicoResult.setdefault("frontier",frontier)
-	dicoResult.setdefault("exploredPages",exploredPages)
-	dicoResult.setdefault("listePagesStatus",listePagesStatus)
-	dicoResult.setdefault("listErrorPages",listErrorPages)
-	dicoResult.setdefault("counterNT",counterNT)
-	dicoResult.setdefault("domainLimit",domainLimit)
-	dicoResult.setdefault("addressRobot",addressRobot)
-	dicoResult.setdefault("crawlDelay",crawlDelay)
-	dicoResult.setdefault("targetLanguage",targetLanguage)
-	return dicoResult
-
+    state=readFile("savedState.txt")
+    #Load frontier
+    listFrontier = search('--Frontier--\n(.*)--Explored pages--\n', state,DOTALL).group(1).split('\n')[:-1]
+    frontier=[]
+    for elem in listFrontier:
+        score= int(elem[1:-2].split(",")[0])
+        page= elem[1:-2].split(",")[1][2:]
+        heapq.heappush(frontier,(score,page))
+    exploredPages = search('--Explored pages--\n(.*)--List of pages status--\n', state,DOTALL).group(1).split('\n')[:-1] #list of links
+    rawListePagesStatus = search('--List of pages status--\n(.*)--List of error pages--\n', state,DOTALL).group(1).split('\n')[:-1]
+    listePagesStatus =[] #List of (link,status)
+    for elem in rawListePagesStatus:
+        link= elem[2:-2].split(",")[0][:-1]
+        status= elem[2:-2].split(",")[1][2:]
+        listePagesStatus.append((link,status))
+    listErrorPages=search('--List of error pages--\n(.*)--Counter of non-translated--\n', state,DOTALL).group(1).split('\n')[:-1] #List of links
+    counterNT=int(search('--Counter of non-translated--\n(.*)--Limit of domain to crawl--\n', state,DOTALL).group(1))
+    domainLimit = search('--Limit of domain to crawl--\n(.*)\n--Robots file address--\n', state,DOTALL).group(1) #String (url)
+    addressRobot = search('--Robots file address--\n(.*)\n--Crawl delay chosen--\n', state,DOTALL).group(1) #String (url)
+    crawlDelay=int(search('--Crawl delay chosen--\n(.*)\n--Target language--\n', state,DOTALL).group(1))
+    targetLanguage = search('--Target language--\n(.*)', state,DOTALL).group(1) #String 
+    #Create Dict and add return
+    dicoResult = dict()
+    dicoResult.setdefault("frontier",frontier)
+    dicoResult.setdefault("exploredPages",exploredPages)
+    dicoResult.setdefault("listePagesStatus",listePagesStatus)
+    dicoResult.setdefault("listErrorPages",listErrorPages)
+    dicoResult.setdefault("counterNT",counterNT)
+    dicoResult.setdefault("domainLimit",domainLimit)
+    dicoResult.setdefault("addressRobot",addressRobot)
+    dicoResult.setdefault("crawlDelay",crawlDelay)
+    dicoResult.setdefault("targetLanguage",targetLanguage)
+    return dicoResult
 
 def getContentInside(content): 
-	return re.compile('<body(.*?)</body>', re.DOTALL).findall(content)
+    return compile('<body(.*?)</body>', DOTALL).findall(content)
 
-def hasBeenTranslated(content,targetLanguage):
-	'''Change here for another setting
-	in our case we check if the list "translation" has targetLanguage'''
-	interestingPart = re.compile('(?<=\<form class="languages go" method="get")(.*?)(?=</form\>)',re.DOTALL).findall(content)
-	if len(interestingPart)>0:
-		return (targetLanguage in interestingPart[0])
-	else:
-		return False
+def hasBeenTranslated(content, targetLanguage):
+    '''Change here for another setting
+    in our case we check if the list "translation" has targetLanguage'''
+    interestingPart = compile('(?<=\<form class="languages go" method="get")(.*?)(?=</form\>)', DOTALL).findall(content)
+    if len(interestingPart)>0:
+        return (targetLanguage in interestingPart[0])
+    else:
+        return False
--- script_crawler.py
+++ script_crawler.py
@@ -1,167 +1,180 @@
-import re
-import sys 
-import string
-import urllib.parse
-import urllib.request
-import heapq
-import time
-import os.path
+#!/usr/bin/env python
+"""This script can be used to find non-translated pages on a domain from a 
+crawl starting with a given URL."""
 
+from sys             import argv
+from urllib.parse    import urlparse, urljoin, quote_plus
+from urllib.request  import urlopen
+from urllib          import robotparser, error
+from heapq           import heappush, heappop
+from time            import sleep
+from os              import path
 #Other file
-import function_crawler
+from function_crawler import (isFetchable,
+                            getListLinks,
+                            computeDepth,
+                            filteredLink,
+                            saveFile,
+                            saveState,
+                            loadState,
+                            getContentInside,
+                            hasBeenTranslated)
 
-if len(sys.argv)!=4:
-	if (len(sys.argv)==2) and (sys.argv[1]=="resume"):
-		print('Resuming previous crawl...')
-	else:
-		print('The first argument should be the source URL of the domain to analyze.')
-		print('The second one is the name of the language (see hasBeenTranslated/isNotOrphan method)')
-		print('The third is the crawl_delay in the robots.txt')
-		exit() 
+
+if len(argv)!=4:
+    if (len(argv) == 2) and (argv[1] == "resume"):
+        print('Resuming previous crawl...')
+    else:
+        print('The first argument is the source URL of the domain to analyze.')
+        print('The second one is the name of the target language ')
+        print('The third is the crawl_delay in the robots.txt')
+        exit() 
 
 
-frontier = []
-exploredPages = []
-listePagesStatus =[]
-listErrorPages=[]
-counterNT=0 #Counter of non-translated pages
-domainLimit=""
-addressRobot=""
-agentName = 'MDN_translation_analysis'
-crawlDelay = 0
-targetLanguage =""
-rp=urllib.robotparser.RobotFileParser()
+frontier         = [] # Frontier of the graph to explore
+exploredPages    = [] # Set of explored pages
+listePagesStatus = [] # Set of tuples with pages and status
+listErrorPages   = [] # List of pages with error
+counterNT        = 0  # Counter of non-translated pages
+domainLimit      = "" # Limit of the domain to explore
+addressRobot     = "" # Address of the robots.txt file
+agentName        = 'MDN_translation_analysis' #Name of the agent we will use
+crawlDelay       = 0  # Delay between two crawls (see robots.txt)
+targetLanguage   = "" # Language that we want to check if it is translated
+rp               = robotparser.RobotFileParser() # I Robot
 
-if(len(sys.argv)==4): #First run
-	sourceURL = sys.argv[1]
-	targetLanguage = sys.argv[2]
-	crawlDelay = int(sys.argv[3])
+if(len(argv) == 4): #First run
+    sourceURL = argv[1]
+    targetLanguage = argv[2]
+    crawlDelay = int(argv[3])
 
-	print('--------------------------------------')
-	print('The origin URL address is :'+ sourceURL)
-	print('The name of the agent is : '+agentName)
-	print('--------------------------------------')
+    print('--------------------------------------')
+    print('The origin URL address is :'+ sourceURL)
+    print('The name of the agent is : '+agentName)
+    print('--------------------------------------')
 
-	tuple1 = (0,sourceURL)
-	heapq.heappush(frontier,tuple1)
-	domainLimit = urllib.parse.urlparse(sourceURL).scheme+"://"+urllib.parse.urlparse(sourceURL).netloc+urllib.parse.urlparse(sourceURL).path
-	#Setting up robots according to robots.txt
-	addressRobot=urllib.parse.urlparse(sourceURL).scheme+"://"+urllib.parse.urlparse(sourceURL).netloc+"/robots.txt"
-	rp.set_url(addressRobot)
-	rp.read()
+    tuple1 = (0, sourceURL)
+    heappush(frontier, tuple1)
+    scheme = urlparse(sourceURL).scheme
+    netloc = urlparse(sourceURL).netloc
+    path = urlparse(sourceURL).path
+    domainLimit = scheme+"://"+netloc+path
+    #Setting up robots according to robots.txt
+    addressRobot = scheme+"://"+netloc+"/robots.txt"
+    rp.set_url(addressRobot)
+    rp.read()
 else: #Resume a previous run
-	#Check if there is the save file
-	if not os.path.isfile("savedState.txt"):
-		print('If you want to resume a previous crawl')
-		print('be sure to have the save file. It should')
-		print('be saved here under "savedState.txt"')
-		exit()
+    #Check if there is the save file
+    if not path.isfile("savedState.txt"):
+        print('If you want to resume a previous crawl')
+        print('be sure to have the save file. It should')
+        print('be saved here under "savedState.txt"')
+        exit()
 
-	#We can continue everything seems ok
-	dictoResult=function_crawler.loadState(frontier,exploredPages,listePagesStatus,listErrorPages,counterNT,domainLimit,addressRobot,crawlDelay,targetLanguage)
-	frontier=dictoResult['frontier']
-	exploredPages=dictoResult['exploredPages']
-	listePagesStatus=dictoResult['listePagesStatus']
-	listErrorPages=dictoResult['listErrorPages']
-	counterNT=dictoResult['counterNT']
-	domainLimit=dictoResult['domainLimit']
-	addressRobot=dictoResult['addressRobot']
-	crawlDelay=dictoResult['crawlDelay']
-	targetLanguage=dictoResult['targetLanguage']
-	rp.set_url(addressRobot)
-	rp.read()
+    #We can continue everything seems ok
+    dictoResult = loadState(frontier, exploredPages, listePagesStatus, listErrorPages, counterNT, domainLimit, addressRobot, crawlDelay, targetLanguage)
+    frontier = dictoResult['frontier']
+    exploredPages = dictoResult['exploredPages']
+    listePagesStatus = dictoResult['listePagesStatus']
+    listErrorPages = dictoResult['listErrorPages']
+    counterNT = dictoResult['counterNT']
+    domainLimit = dictoResult['domainLimit']
+    addressRobot = dictoResult['addressRobot']
+    crawlDelay = dictoResult['crawlDelay']
+    targetLanguage = dictoResult['targetLanguage']
+    rp.set_url(addressRobot)
+    rp.read()
 
-indexLoop=0
+indexLoop = 0
 #Loop
 while frontier :
-	if (indexLoop % 100==0 and indexLoop>0): #Save
-		function_crawler.saveState(frontier,exploredPages,listePagesStatus,listErrorPages,counterNT,domainLimit,addressRobot,crawlDelay,targetLanguage)
-	#webpage to explore
-	tupleCurrent = heapq.heappop(frontier)
-	#The tuple is composed of score + url
-	currentPageToExplore = tupleCurrent[1]
-	if currentPageToExplore not in exploredPages:
-		#Handle 404 error : make sure that connection was not interrupted to be certain of result consistency 
-		try:
-			urlRequest = urllib.request.urlopen(urllib.parse.quote_plus(currentPageToExplore,"/:"))
-			#the input argument has to have the same scheme (e.g. http -> https won't work)
-			#Cut the  URL with possible "?redirect"
-			currentPageToExplore=urlRequest.geturl().split('?redirect')[0]
-			if currentPageToExplore not in exploredPages:
-				print('Page : '+currentPageToExplore)
-				#get a list of things inside body	
-				contentPage=urlRequest.read().decode('utf-8')
-				listContent = function_crawler.getContentInside(contentPage)
+    if (indexLoop % 100 == 0 and indexLoop>0): #Save
+        saveState(frontier, exploredPages, listePagesStatus, listErrorPages, counterNT, domainLimit, addressRobot, crawlDelay, targetLanguage)
+    #webpage to explore
+    tupleCurrent = heappop(frontier)
+    #The tuple is composed of score + url
+    currentPageToExplore = tupleCurrent[1]
+    if currentPageToExplore not in exploredPages:
+        #Handle 404 error : make sure that connection was not interrupted to be certain of result consistency 
+        try:
+            quotedURL = quote_plus(currentPageToExplore, "/:")
+            urlRequest = urlopen(quotedURL)
+            #the input argument has to have the same scheme (e.g. http -> https won't work)
+            #Cut the  URL with possible "?redirect"
+            currentPageToExplore = urlRequest.geturl().split('?redirect')[0]
+            if (currentPageToExplore not in exploredPages) and (currentPageToExplore.startswith(domainLimit)):
+                print('Page : '+currentPageToExplore)
+                #get a list of things inside body
+                contentPage = urlRequest.read().decode('utf-8')
+                listContent = getContentInside(contentPage)
 
-				#loop (maybe many pairs of listContent... who knows)
-				list_links = []
-				for c in listContent:
-					list_links += function_crawler.getListLinks(c)
+                #loop (maybe many pairs of listContent... who knows)
+                list_links = []
+                for c in listContent:
+                    list_links += getListLinks(c)
 
-				#Now we have every link : make them absolute considering the current webpage 
-				#in case we may encounter other directories
-				listAbsolute=[]	
-				for link in list_links:
-					link = urllib.parse.urljoin(currentPageToExplore,link)
-					linkParseResult = urllib.parse.urlparse(link)
-					#avoid anchor repetition
-					link=linkParseResult.scheme+"://"+linkParseResult.netloc+linkParseResult.path
-					listAbsolute.append(link)
-				
-				for i,link in enumerate(listAbsolute):
-					#print(link)
-					#Do we want to access this link ? (robots.txt+ inside the domain ?)
-					if not function_crawler.isFetchable(link, rp, agentName, domainLimit):
-						listAbsolute.remove(link)
-						list_links.pop(i)
-					
-					#Is this link already known ?
-					elif link in exploredPages or link==currentPageToExplore:
-						listAbsolute.remove(link)
-						list_links.pop(i)
-						#Compute score 
-					else:
-						scoreLink = function_crawler.computeDepth(link)
-						tupleScored = (scoreLink,link)
-						#Already in the frontier ? 
-						if tupleScored in frontier:
-							listAbsolute.remove(link)
-							list_links.pop(i)
-						#Filter some links
-						elif function_crawler.filteredLink(link):
-							listAbsolute.remove(link)
-							list_links.pop(i)	
-						#Else add the tuple to the frontier
-						else:
-							heapq.heappush(frontier,tupleScored)
-							#print "correct link added : "+link
+                #Now we have every link : make them absolute considering the current webpage 
+                #in case we may encounter other directories
+                listAbsolute = []    
+                for link in list_links:
+                    link = urljoin(currentPageToExplore, link)
+                    linkParseResult = urlparse(link)
+                    #avoid anchor repetition
+                    link = linkParseResult.scheme+"://"+linkParseResult.netloc
+                    link = link+linkParseResult.path
+                    listAbsolute.append(link)
+                for i, link in enumerate(listAbsolute):
+                    #Do we want to access this link ? (robots.txt+ inside the domain ?)
+                    if not isFetchable(link,  rp,  agentName,  domainLimit):
+                        listAbsolute.remove(link)
+                        list_links.pop(i)
+                    #Is this link already known ?
+                    elif link in exploredPages or link == currentPageToExplore:
+                        listAbsolute.remove(link)
+                        list_links.pop(i)
+                    #Compute score 
+                    else:
+                        scoreLink = computeDepth(link)
+                        tupleScored = (scoreLink, link)
+                        #Already in the frontier ? 
+                        if tupleScored in frontier:
+                            listAbsolute.remove(link)
+                            list_links.pop(i)
+                        #Filter some links
+                        elif filteredLink(link):
+                            listAbsolute.remove(link)
+                            list_links.pop(i)
+                        #Else add the tuple to the frontier
+                        else:
+                            heappush(frontier, tupleScored)
+                exploredPages.append(currentPageToExplore)
+                print("Size frontier : "+str(len(frontier)))
+                print("# of explored pages : "+str(len(exploredPages)))
+                indexLoop = indexLoop+1
+                #Is the current page translated ? 
+                if hasBeenTranslated(contentPage, targetLanguage):
+                    listePagesStatus.append((currentPageToExplore, "translated"))
+                else:
+                    print("non translated page found : "+currentPageToExplore)
+                    listePagesStatus.append((currentPageToExplore, "non-translated"))
+                    counterNT = counterNT+1
+            #Wait because of crawl/delay and ratio/request as far as
+            #we don't have multiple threads/crawlers
+            sleep(crawlDelay)
+        except (error.HTTPError, error.URLError):
+            print("Urllib error (?Connection?):"+currentPageToExplore)
+            listErrorPages.append(currentPageToExplore)
+        except UnicodeEncodeError:
+            print("Encoding error :"+currentPageToExplore)
+            listErrorPages.append(currentPageToExplore)
 
-				exploredPages.append(currentPageToExplore)
-				print("Size frontier : "+str(len(frontier)))
-				print("# of explored pages : "+str(len(exploredPages)))
-				indexLoop=indexLoop+1
-				#Is the current page translated ? 
-				if function_crawler.hasBeenTranslated(contentPage,targetLanguage):
-					listePagesStatus.append((currentPageToExplore,"translated"))
-				else:
-					print("non translated page found : "+currentPageToExplore)
-					listePagesStatus.append((currentPageToExplore,"non-translated"))
-					counterNT=counterNT+1
-				#Wait because of crawl/delay and ratio/request as far as
-				#we don't have multiple threads/crawlers
-				time.sleep(crawlDelay)
-		except (urllib.error.HTTPError,urllib.error.URLError):
-			print("Urllib error (?Connection?):"+currentPageToExplore)
-			listErrorPages.append(currentPageToExplore)
-		except UnicodeEncodeError:
-			print("Encoding error :"+currentPageToExplore)
-			listErrorPages.append(currentPageToExplore)
-
-'''for page in exploredPages:
-	print(page)'''
-#print(listePagesStatus)
-function_crawler.saveFile(listePagesStatus,"log_crawl.txt")
-function_crawler.saveFile(listErrorPages,"log_error_pages.txt")
+#Transform listePagesStatus so that generate_html.py is ok
+finalListStatus=[]
+for tupleStatus in listePagesStatus:
+    finalListStatus.append(tupleStatus[0]+", "+tupleStatus[1])
+finalListStatus.sort(key=str.lower)
+saveFile(finalListStatus,"log_crawl.txt")
+saveFile(listErrorPages,"log_error_pages.txt")
 print("Number of non-translated pages: "+str(counterNT))
 print("Number of pages with error: "+str(len(listErrorPages)))
 print("End of script : files saved under log_crawl.txt and log_error_pages.txt")
